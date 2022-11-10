84b1289aef1780397cefd7901045a49bf86b1ed2
zaid239 <zaidi.workspace@gmail.com>
2022-11-10 18:09:28 +0500

    chnages to ts | added font to page

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Seo from '../src/components/Seo'
+
+export default function Home() {
+  return (
+   <div className='container mx-auto flex min-h-screen flex-col px-4 sm:px-6'>
+    <Seo templateTitle='Home'/>
+    <div className='m-auto'>
+
+    <h1 className='bg-transparent main text-4xl sm:text-6xl text-center  text-white'>DEVCOD</h1>
+
+    <p className='text-white text-center text-md sm:text-xl my-8 msg'>---------Coming Soon---------</p>
+    </div>
+   </div>
+  )
+}
